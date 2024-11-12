2d50ac8eb6ae6a13d56d48c3451a9ab92c65d447
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-12 17:15:40 +0100

    feat: add Snack 2

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 
 // Creo un arrey di oggetti
 const biciList = [
-    { nome: `Vento`, peso: 7.8 },
-    { nome: `Roccia`, peso: 12.5 },
-    { nome: `Aquila`, peso: 8.3 },
-    { nome: `Fiamma`, peso: 6.9 },
-    { nome: `Scatto`, peso: 9.1 }
+    {nome: `Vento`, peso: 7.8},
+    {nome: `Roccia`, peso: 12.5},
+    {nome: `Aquila`, peso: 8.3},
+    {nome: `Fiamma`, peso: 6.9},
+    {nome: `Scatto`, peso: 9.1}
 ]
 console.log(biciList);
 
@@ -26,3 +26,52 @@ for (let i = 0; i < biciList.length; i++) {
 
 // Stampo il risultato in console
 console.log(`La bici più leggera è la ${lighterBici.nome} con un peso di ${lighterBici.peso}Kg.`);
+
+
+// Snack 2
+
+// Creo un arrey di oggetti
+const team = [
+    {nome: `Napoli`, punti: 0, goal: 0},
+    {nome: `Inter`, punti: 0, goal: 0},
+    {nome: `Atalanta`, punti: 0, goal: 0},
+    {nome: `Lazio`, punti: 0, goal: 0},
+    {nome: `Juventus`, punti: 0, goal: 0},
+    {nome: `Milan`, punti: 0, goal: 0},
+    {nome: `Roma`, punti: 0, goal: 0},
+]
+console.log(team);
+
+// Creo una funzioni di appoggio che genera num. casuali
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min)
+
+// Creo tre variabili generali 
+const newArray = [];
+let randomPoints;
+let randomGoals;
+
+// Creo un ciclo for per scorrere nella lista degli oggetti
+for (let i = 0; i < team.length; i++) {
+    let currentTeam = team[i];
+    // console.log(currentTeam);
+
+    // Assegno un numero casuale da 0 a 100 alle variabili di appoggio
+    randomPoints = getRandomInt(0, 100)
+    randomGoals = getRandomInt(0, 100)
+    // console.log(randomPoints, randomGoals);
+
+    // Assegno quel numero casuale alle keys degli oggetti nell'array
+    currentTeam.punti = randomPoints
+    currentTeam.goal = randomGoals
+    console.log(currentTeam);
+
+    // Creo un nuovo array con solo due keys: nome e punti delle squadre
+    newArray.push(currentTeam.nome, currentTeam.punti)
+    // console.log(newArray);
+    
+}
+
+// Stampo il risultato in console
+console.log(newArray);
+
+
